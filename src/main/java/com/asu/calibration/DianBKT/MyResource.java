1a5cbcc8dab572932ca0f0596ed416b150e08578
Lakshmisagar Hiragappa kusnoor (Student) <lkusnoor@sundevils.asu.edu>
2018-01-27 02:10:56 -0700

    inital commit 2

--- src/main/java/com/asu/calibration/DianBKT/MyResource.java
+++ src/main/java/com/asu/calibration/DianBKT/MyResource.java
@@ -20,6 +20,10 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "Got it!";
+        
+    	
+    	return "Got it!";
+        
+        
     }
 }
